02d903d748b725586272feedf8bba84f6c26f087
TrHuongIT <tramhuongptit@gmail.com>
2023-03-21 18:02:04 +0700

    Lab6

---
+++ Lab06/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06
+{
+    internal class Class1
+    {
+        public void ContentClass1()
+        {
+            byte[] a = new byte[5];
+
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write("a[{0}]=", i + 1);
+                    a[i] = Convert.ToByte(Console.ReadLine());
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Khong duoc nhap ky tu cho mang so");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Khong duoc nhap gia tri nam ngoai mang 0-255");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Loi vuot qua pham vi cua mang");
+            }
+
+            Console.WriteLine("Mang vua nhap la: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(" {0}", a[i]);
+            }
+        }
+    }
+}
---
+++ Lab06/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06
+{
+    public class InvalidInputNumber : Exception
+    {
+        public InvalidInputNumber():base("Hay nhap mot so lon hon 0"){}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 content = new Class1();
+            content.ContentClass1();
+
+            int intCnt;
+            int intNum = 0;
+
+            Console.Write("Nhap mot so bat ky:");
+
+            try
+            {
+                intNum = int.Parse(Console.ReadLine());
+
+                if (intNum <= 0)
+                {
+                    throw new InvalidInputNumber();
+                }
+            }
+            catch(InvalidInputNumber objInvalidInput)
+            {
+                Console.WriteLine(objInvalidInput.Message);
+            }
+            catch(System.FormatException objFormatException)
+            {
+                Console.WriteLine(objFormatException.Message);
+            }
+            finally
+            {
+                if (intNum > 0)
+                {
+                    for (intCnt = 1; intCnt <= 10; intCnt++)
+                    {
+                        Console.WriteLine(intCnt*intNum);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
